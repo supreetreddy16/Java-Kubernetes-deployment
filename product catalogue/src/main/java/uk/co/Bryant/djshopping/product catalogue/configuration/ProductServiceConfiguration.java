a645a2cf8bcfd76c74d023abf2b8af783eba942c
supreetreddy <145430159+supreetreddy16@users.noreply.github.com>
2024-04-08 17:02:16 -0500

    Updated ProductServiceConfiguration

--- product catalogue/src/main/java/uk/co/Bryant/djshopping/product catalogue/configuration/ProductServiceConfiguration.java
+++ product catalogue/src/main/java/uk/co/Bryant/djshopping/product catalogue/configuration/ProductServiceConfiguration.java
@@ -1,4 +1,4 @@
-package uk.co.danielbryant.djshopping.productcatalogue.configuration;
+package uk.co.bryant.djshopping.productcatalogue.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
